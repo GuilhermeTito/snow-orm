29123fb6736e2f394fa34247ee33c4013ab2c80a
Guilherme Ferraz Ronha <guilhermeferrazronha123@gmail.com>
2024-07-09 19:39:36 -0300

    feat: table and column attributes

---
+++ Mapping/SnowORM.Mapping.Attributes.pas
@@ -0,0 +1,65 @@
+unit SnowORM.Mapping.Attributes;
+
+interface
+
+uses SnowORM.Mapping.Properties;
+
+type
+  Table = class(TCustomAttribute)
+    private
+      FName: string;
+    public
+      constructor Create(const AName: string);
+      property Name: string read FName;
+  end;
+
+  Column = class(TCustomAttribute)
+    private
+      FName: string;
+      FUnique: Boolean;
+      FRequired: Boolean;
+      FNoInsert: Boolean;
+      FNoUpdate: Boolean;
+    public
+      constructor Create(const AName: string); overload;
+      constructor Create(const AName: string; const AProperties: TSnowColumnProperties); overload;
+      property Name: string read FName;
+      property Unique: Boolean read FUnique default False;
+      property Required: Boolean read FRequired default False;
+      property NoInsert: Boolean read FNoInsert default False;
+      property NoUpdate: Boolean read FNoUpdate default False;
+  end;
+
+implementation
+
+{ Table }
+
+constructor Table.Create(const AName: string);
+begin
+  FName := AName;
+end;
+
+{ Column }
+
+constructor Column.Create(const AName: string;
+  const AProperties: TSnowColumnProperties);
+var
+  LProperty: TSnowColumnProperty;
+begin
+  for LProperty in AProperties do
+  begin
+    case LProperty of
+      scpUnique: FUnique := True;
+      scpRequired: FRequired := True;
+      scpNoInsert: FNoInsert := True;
+      scpNoUpdate: FNoUpdate := True;
+    end;
+  end;
+end;
+
+constructor Column.Create(const AName: string);
+begin
+  FName := AName;
+end;
+
+end.
---
+++ Mapping/SnowORM.Mapping.Properties.pas
@@ -0,0 +1,12 @@
+unit SnowORM.Mapping.Properties;
+
+interface
+
+type
+  TSnowColumnProperty = (scpUnique, scpRequired, scpNoInsert, scpNoUpdate);
+
+  TSnowColumnProperties = set of TSnowColumnProperty;
+
+implementation
+
+end.
