8893f7f0ae8b453a3d009d5f414692d3a4f06907
Guilherme Ferraz Ronha <guilhermeferrazronha123@gmail.com>
2024-08-16 21:49:07 -0300

    feat: added insert method to dao

--- Data/SnowORM.Data.DAO.pas
+++ Data/SnowORM.Data.DAO.pas
@@ -12,6 +12,7 @@ type
   public
     constructor Create(AConnection: ISnowConnection);
     procedure Find<T: class>(Entity: T; const AFilter: string = '');
+    procedure Insert<T: class>(Entity: T);
   end;
 
 implementation
@@ -34,6 +35,7 @@ var
   LProperty: TRttiProperty;
 begin
   LContext := TRttiContext.Create;
+
   try
     LType := LContext.GetType(T);
 
@@ -46,36 +48,78 @@ begin
       end;
     end;
 
+    FTable.Filtered(True);
     FTable.Filter(AFilter);
-
     FTable.Open;
 
     if FTable.RecordCount > 0 then
     begin
-      FTable.First;
-
-      while not FTable.Eof do
+      for LProperty in LType.GetProperties do
       begin
-        for LProperty in LType.GetProperties do
+        for LAttribute in LProperty.GetAttributes do
         begin
-          for LAttribute in LProperty.GetAttributes do
+          if LAttribute is Column then
           begin
-            if LAttribute is Column then
-            begin
-              if Assigned(FTable.FindField(Column(LAttribute).Name)) then
-                LProperty.SetValue(TObject(Entity), FieldToTValue(FTable.FieldByName(Column(LAttribute).Name)));
+            if Assigned(FTable.FindField(Column(LAttribute).Name)) then
+              LProperty.SetValue(TObject(Entity), FieldToTValue(FTable.FieldByName(Column(LAttribute).Name)));
 
-              Break;
-            end;
+            Break;
           end;
         end;
-
-        FTable.Next;
       end;
     end;
   finally
     LContext.Free;
     FTable.Close;
+    FTable.TableName('');
+    FTable.Filtered(False);
+    FTable.Filter('');
+  end;
+end;
+
+procedure TSnowDAO.Insert<T>(Entity: T);
+var
+  LContext: TRttiContext;
+  LType: TRttiType;
+  LAttribute: TCustomAttribute;
+  LProperty: TRttiProperty;
+begin
+  LContext := TRttiContext.Create;
+
+  try
+    LType := LContext.GetType(T);
+
+    for LAttribute in LType.GetAttributes do
+    begin
+      if LAttribute is Table then
+      begin
+        FTable.TableName(Table(LAttribute).Name);
+        Break;
+      end;
+    end;
+
+    FTable.Open;
+    FTable.Append;
+
+    for LProperty in LType.GetProperties do
+    begin
+      for LAttribute in LProperty.GetAttributes do
+      begin
+        if LAttribute is Column then
+        begin
+          if Assigned(FTable.FindField(Column(LAttribute).Name)) then
+            FTable.FieldByName(Column(LAttribute).Name).Value := LProperty.GetValue(TObject(Entity)).AsVariant;
+
+          Break;
+        end;
+      end;
+    end;
+
+    FTable.Post;
+  finally
+    LContext.Free;
+    FTable.Close;
+    FTable.TableName('');
   end;
 end;
 
