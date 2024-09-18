ca2d7177e6b40aeb73ef9ce5aab5df9b74232cd2
Guilherme Ferraz Ronha <guilhermeferrazronha123@gmail.com>
2024-09-17 21:24:21 -0300

    feat: added an Interface for data fields

---
+++ Types/SnowORM.Types.Interfaces.pas
@@ -0,0 +1,14 @@
+unit SnowORM.Types.Interfaces;
+
+interface
+
+type
+  ISnowField = interface
+    ['{097B9477-0F1C-426D-A83D-207D15F552BF}']
+    procedure Clear;
+    function AsString: string;
+  end;
+
+implementation
+
+end.
